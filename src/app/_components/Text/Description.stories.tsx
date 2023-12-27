49b2f036eeefca48c59ba0de2f397305bad7a622
Yumax-panda <yuma0wall@gmail.com>
2023-12-28 02:46:03 +0900

    storybookを追加

---
+++ src/app/_components/Text/Description.stories.tsx
@@ -0,0 +1,21 @@
+import { StoryObj, Meta } from '@storybook/react'
+
+import { Description } from './Description'
+
+export default {
+  title: 'Text/Description',
+  component: Description,
+  argTypes: {
+    children: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+} as Meta<typeof Description>
+
+type Story = StoryObj<typeof Description>
+
+export const Basic: Story = {
+  args: { description: 'test' },
+}
