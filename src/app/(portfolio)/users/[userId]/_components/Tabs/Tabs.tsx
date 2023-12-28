3a23d2a9648fdfa5c4ddffadaa4cc41668efb164
Yumax-panda <yuma0wall@gmail.com>
2023-12-28 21:11:04 +0900

    ナビゲーションタブを実装

---
+++ src/app/(portfolio)/users/[userId]/_components/Tabs/Tabs.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { usePathname, useRouter } from 'next/navigation'
+
+import { routes } from '@/lib/routes'
+import { Tab, Tabs as MuiTabs } from '@mui/material'
+
+type Props = {
+  userId: string
+}
+
+export const Tabs = ({ userId }: Props) => {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  const pathes = {
+    技術: routes.userSkill(userId),
+    制作物: routes.userWork(userId),
+    執筆記事: routes.userArticle(userId),
+  }
+  const handleChange = (_: React.SyntheticEvent, href: string) => {
+    router.push(href)
+  }
+
+  return (
+    <MuiTabs
+      value={pathname}
+      onChange={handleChange}
+      sx={{ borderBottom: 1, borderColor: 'lightgray' }}
+    >
+      {Object.entries(pathes).map(([label, href]) => (
+        <Tab
+          key={href}
+          label={label}
+          value={href}
+          sx={{ fontSize: '1.2rem', mr: '1rem' }}
+        />
+      ))}
+    </MuiTabs>
+  )
+}
