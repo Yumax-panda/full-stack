9dcd8e6f762969eec6c7dbfebae523bbabf6b55d
Yumax-panda <yuma0wall@gmail.com>
2023-12-29 06:45:17 +0900

    Containerの幅を広くしてみやすくした

--- src/app/_components/Container/Container.tsx
+++ src/app/_components/Container/Container.tsx
@@ -1,9 +1,9 @@
-import { Container as MuiContainer, type ContainerProps } from '@mui/material'
+import { Container as MuiContainer, ContainerProps } from '@mui/material'
 
 export const Container = (props: ContainerProps) => {
   const { children, ...rest } = props
   return (
-    <MuiContainer maxWidth='md' {...rest}>
+    <MuiContainer maxWidth='lg' {...rest}>
       {children}
     </MuiContainer>
   )
