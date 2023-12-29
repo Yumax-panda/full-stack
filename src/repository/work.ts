639ff2272f2e20e31a71e2514a3b51e0f2975db9
Yumax-panda <yuma0wall@gmail.com>
2023-12-29 18:52:59 +0900

    制作物取得のメソッドを追加

--- src/repository/work.ts
+++ src/repository/work.ts
@@ -2,6 +2,31 @@ import { prisma } from '@/lib/client'
 
 import type { Work } from '@prisma/client'
 
+export type PartialWork = Pick<Work, 'id' | 'title' | 'thumbnail'>
+
+export async function getPartialWorksByUserId(
+  userId: string,
+): Promise<PartialWork[]> {
+  return prisma.work.findMany({
+    where: {
+      userId,
+    },
+    select: {
+      id: true,
+      title: true,
+      thumbnail: true,
+    },
+  })
+}
+
+export async function getWorkById(workId: string): Promise<Work | null> {
+  return prisma.work.findUnique({
+    where: {
+      id: workId,
+    },
+  })
+}
+
 export async function getWorksByUserId(userId: string): Promise<Work[]> {
   return prisma.work.findMany({
     where: {
