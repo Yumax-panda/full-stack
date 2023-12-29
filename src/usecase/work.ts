89fc34127bae7bec2af5678124427aa42bbdc397
Yumax-panda <yuma0wall@gmail.com>
2023-12-29 18:24:37 +0900

    制作物記事の新規追加ロジックを作成

--- src/usecase/work.ts
+++ src/usecase/work.ts
@@ -1,1 +1,15 @@
 import 'server-only'
+
+import { createNewWork, getEmptyWork } from '@/repository/work'
+
+import type { Work } from '@prisma/client'
+
+export async function getOrCreateEmptyWork(userId: string): Promise<Work> {
+  const emptyWork = await getEmptyWork(userId)
+
+  if (emptyWork) {
+    return emptyWork
+  }
+
+  return createNewWork(userId)
+}
