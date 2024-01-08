e1c44bd0514963af3fff00d9e0dba462cf57e210
Yumax-panda <yuma0wall@gmail.com>
2024-01-09 02:58:45 +0900

    不要な定義を削除

--- src/models/user.ts
+++
@@ -1,9 +0,0 @@
-export type User = {
-  id: string
-  name: string | null
-  email: string | null
-  emailVerified: Date | null
-  image: string | null
-  location: string | null
-  organization: string | null
-}
