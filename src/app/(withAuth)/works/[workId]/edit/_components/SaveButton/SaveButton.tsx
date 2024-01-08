6ca632820cd514a6d907e2c8354516a16aa89d93
Yumax-panda <yuma0wall@gmail.com>
2024-01-09 00:37:19 +0900

    onSubmitのバリデーションが効くように修正

--- src/app/(withAuth)/works/[workId]/edit/_components/SaveButton/SaveButton.tsx
+++ src/app/(withAuth)/works/[workId]/edit/_components/SaveButton/SaveButton.tsx
@@ -2,12 +2,13 @@ import { SaveAltOutlined } from '@mui/icons-material'
 import { IconButton, Tooltip } from '@mui/material'
 
 type Props = {
-  onClick: () => void
+  onSubmit: () => void
+  disabled?: boolean
 }
 
-export const SaveButton = ({ onClick }: Props) => (
+export const SaveButton = ({ onSubmit }: Props) => (
   <Tooltip title="保存" placement='right-start'>
-    <IconButton onClick={onClick}>
+    <IconButton onSubmit={onSubmit} type='submit'>
       <SaveAltOutlined />
     </IconButton>
   </Tooltip>
