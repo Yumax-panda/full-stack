2a7c412e552c890cfe6cbd27558d4cca7037e519
Yumax-panda <yuma0wall@gmail.com>
2024-01-09 22:33:09 +0900

    テストケースの追加

---
+++ src/__tests__/work.test.ts
@@ -0,0 +1,31 @@
+import { expect, test } from 'vitest'
+
+import { isEmpty } from '@/models'
+
+test('isEmpty <div></div>', () => {
+  expect(isEmpty('<div></div>')).toBe(true)
+})
+
+test("isEmpty ''", () => {
+  expect(isEmpty('')).toBe(true)
+})
+
+test('isEmpty null', () => {
+  expect(isEmpty(null)).toBe(true)
+})
+
+test('isEmpty undefined', () => {
+  expect(isEmpty(undefined)).toBe(true)
+})
+
+test('isEmpty <div>test</div>', () => {
+  expect(isEmpty('<div>test</div>')).toBe(false)
+})
+
+test('isEmpty nested empty div', () => {
+  expect(isEmpty('<div><div></div></div>')).toBe(true)
+})
+
+test('isEmpty nested div', () => {
+  expect(isEmpty('<div><div>test</div></div>')).toBe(false)
+})
