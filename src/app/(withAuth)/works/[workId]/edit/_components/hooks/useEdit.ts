f992e8b25356f3f2770ac92910f3a79caeb7c4fb
Yumax-panda <yuma0wall@gmail.com>
2024-01-11 01:37:02 +0900

    フロント側のロジックを実装

--- src/app/(withAuth)/works/[workId]/edit/_components/hooks/useEdit.ts
+++ src/app/(withAuth)/works/[workId]/edit/_components/hooks/useEdit.ts
@@ -1,4 +1,5 @@
 import type { Control, FormState } from 'react-hook-form'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 import { updateWorkSchema as formSchema } from '@/models'
@@ -7,7 +8,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 
 import type { Work as Props } from '@prisma/client'
 
-import type { UpdateWork as FormValues } from '@/models'
+import type { UpdateWork as FormValues, UpdateWork } from '@/models'
 type UseEditReturn = {
   control: Control<FormValues>
   onSubmit: () => void
@@ -22,9 +23,15 @@ type UseEditReturn = {
 export const useEdit = ({
   title,
   content,
-  thumbnail: initialThumbnail,
+  thumbnail: _initialThumbnail,
   ...rest
 }: Props): UseEditReturn => {
+  const [thumbnailFile, setThumbnailFile] = useState<File | null>(null)
+  // 初期値はnullにしておく初期のサムネイル画像がある場合はそれをセットする
+  const [initialThumbnail, setInitialThumbnail] = useState<string | null>(
+    _initialThumbnail,
+  )
+
   const { control, handleSubmit, formState, watch, setValue } =
     useForm<FormValues>({
       mode: 'onChange',
@@ -32,20 +39,23 @@ export const useEdit = ({
       defaultValues: {
         title: title ?? '',
         content: content ?? '',
-        thumbnail: initialThumbnail ?? '',
+        thumbnail: '',
         ...rest,
       },
     })
+
+  useEffect(() => {
+    if (initialThumbnail) {
+      setValue('thumbnail', initialThumbnail ?? '')
+    }
+  }, [initialThumbnail, setValue])
 
   const onThumbnailUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0]
     if (!file) return
 
-    const url = await workImageStorage.upload({
-      userId: rest.userId,
-      workId: rest.id,
-      file,
-    })
+    setThumbnailFile(file)
+    const url = URL.createObjectURL(file)
 
     setValue('thumbnail', url)
   }
@@ -55,12 +65,52 @@ export const useEdit = ({
   const onThumbnailRemove = async () => {
     if (!thumbnail) return
 
-    await workImageStorage.delete(thumbnail)
+    setThumbnailFile(null)
     setValue('thumbnail', null)
   }
 
-  const onSubmit = handleSubmit((data) => {
-    console.log(data)
+  const onSubmit = handleSubmit(async (data) => {
+    const { id, userId, thumbnail } = data
+
+    // データを値渡しでコピー
+    const updatePayload: UpdateWork = { ...data }
+
+    // 画像に変更がある場合
+    // 1. 画像を削除した場合
+    if (initialThumbnail && !thumbnail) {
+      await workImageStorage.delete(initialThumbnail)
+      updatePayload.thumbnail = null
+      setInitialThumbnail(null)
+    }
+    // 2. 画像を変更した場合 (既存の画像を削除して新しい画像をアップロードする)
+    else if (thumbnailFile) {
+      const tasks: Promise<void>[] = [
+        (async () => {
+          const url = await workImageStorage.upload({
+            file: thumbnailFile,
+            userId,
+            workId: id,
+          })
+          updatePayload.thumbnail = url
+          setInitialThumbnail(url)
+        })(),
+      ]
+      if (initialThumbnail) {
+        tasks.push(workImageStorage.delete(initialThumbnail))
+      }
+      await Promise.all(tasks)
+    }
+
+    console.log('updatePayload', updatePayload)
+
+    // apiに送信
+    // await fetch(`/api/works/${id}`, {
+    //   method: 'PATCH',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(updatePayload),
+    // })
   })
 
   const isPrivate = watch('isPrivate')
