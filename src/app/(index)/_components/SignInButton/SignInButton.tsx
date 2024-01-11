20717d2eb9ff87c6b9d477032551f4d439d885cf
Yumax-panda <yuma0wall@gmail.com>
2024-01-12 04:38:12 +0900

    ログインの有無で文言と処理を分けた

--- src/app/(index)/_components/SignInButton/SignInButton.tsx
+++ src/app/(index)/_components/SignInButton/SignInButton.tsx
@@ -1,20 +1,33 @@
 'use client'
 
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 
+import { routes } from '@/lib/routes'
 import { Button } from '@mui/material'
 
-export const SigninButton = () => (
-  <Button
-    onClick={() => signIn('github')}
-    style={{
-      padding: '0.5rem 1rem',
-      borderRadius: '0.5rem',
-      backgroundColor: 'lightgray',
-      color: 'black',
-      textDecoration: 'none',
-    }}
-  >
-    ログイン / 新規登録
-  </Button>
-)
+export const SigninButton = () => {
+  const { data: session } = useSession()
+  const user = session?.user ?? null
+  const router = useRouter()
+
+  const onClick = user
+    ? () => router.push(routes.userSkill(user.id))
+    : () => signIn('github')
+  const message = user ? 'マイページへ' : 'ログイン / 新規登録'
+
+  return (
+    <Button
+      onClick={onClick}
+      style={{
+        padding: '0.5rem 1rem',
+        borderRadius: '0.5rem',
+        backgroundColor: 'lightgray',
+        color: 'black',
+        textDecoration: 'none',
+      }}
+    >
+      {message}
+    </Button>
+  )
+}
