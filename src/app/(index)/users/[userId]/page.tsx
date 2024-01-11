66d93359ca2d18164277a730bd03e68df73b837d
Yumax-panda <yuma0wall@gmail.com>
2024-01-12 00:42:12 +0900

    グループ名を修正

--- src/app/(index)/users/[userId]/page.tsx
+++ src/app/(index)/users/[userId]/page.tsx
@@ -2,7 +2,11 @@ import { getSkillsWithTagsByUserId } from '@/repository/skill'
 
 import { SkillSection } from './_components/SkillSection'
 
-export default async function Page({ userId }: { userId: string }) {
+export default async function Page({
+  params: { userId },
+}: {
+  params: { userId: string }
+}) {
   const skills = await getSkillsWithTagsByUserId(userId)
 
   return <SkillSection skills={skills} />
