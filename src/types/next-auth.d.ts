bee40fe56456c64bbbbd8e701eed4724e343eb04
Yumax-panda <yuma0wall@gmail.com>
2024-01-12 02:20:21 +0900

    DBのスキーマと型を合わせた

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,8 @@
 import type { DefaultUser } from 'next-auth'
+import type { User } from '@prisma/client'
 
 declare module 'next-auth' {
   interface Session {
-    user?: DefaultUser & {
-      id: string
-    }
+    user?: DefaultUser & User
   }
 }
