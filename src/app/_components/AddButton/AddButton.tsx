c7f89ea144e39267103f4cc44f8a30ab9332bbe7
Yumax-panda <yuma0wall@gmail.com>
2024-01-12 19:49:30 +0900

    フォーマッタ適用

--- src/app/_components/AddButton/AddButton.tsx
+++ src/app/_components/AddButton/AddButton.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import { Add } from '@mui/icons-material'
 import { IconButton, Tooltip } from '@mui/material'
@@ -10,8 +10,12 @@ type Props = {
 
 export const AddButton = ({ onClick, text }: Props) => (
   <Tooltip title={text}>
-    <IconButton color='inherit' onClick={onClick} sx={{ border: "1px solid lightGray" }}>
-      <Add sx={{ color: "GrayText" }} />
+    <IconButton
+      color='inherit'
+      onClick={onClick}
+      sx={{ border: '1px solid lightGray' }}
+    >
+      <Add sx={{ color: 'GrayText' }} />
     </IconButton>
   </Tooltip>
 )
