02f7ea3d15b8c67e3ea13f42597762c35ecc573d
Yumax-panda <yuma0wall@gmail.com>
2024-01-13 05:52:10 +0900

    テーブル完成

--- src/app/(index)/settings/skills/_components/SkillsTable/SkillsTable.tsx
+++ src/app/(index)/settings/skills/_components/SkillsTable/SkillsTable.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 
 import { StarField } from '@/app/(index)/_components/StarField'
 import { Edit } from '@mui/icons-material'
-import { Box, IconButton, Tooltip } from '@mui/material'
+import { Box, Chip, IconButton, Tooltip } from '@mui/material'
 
 import { UpdateSkillForm } from '../UpdateSkillForm'
 
@@ -28,20 +28,52 @@ type EditableTableRowProps = {
 const EditableRow = ({ skill }: EditableTableRowProps) => {
   const [open, setOpen] = useState(false)
   const SkillTableRow = () => (
-    <Box sx={{ display: 'flex', width: '100%' }}>
-      <Box sx={{ width: '20%', pl: '1rem' }}>{skill.name}</Box>
-      <Box sx={{ flexGrow: 1 }}>
-        <StarField level={skill.level} />
+    <>
+      <Box sx={{ width: '20%', pl: '1rem', display: 'flex' }}>
+        <Box sx={{ my: 'auto' }}>{skill.name}</Box>
       </Box>
-      <Box sx={{ flexGrow: 4 }}>
-        {skill.tags.map(({ name }) => name).join(', ')}
+      <Box sx={{ flexGrow: 1, display: 'flex' }}>
+        <Box sx={{ my: 'auto' }}>
+          <StarField level={skill.level} />
+        </Box>
       </Box>
-      <Box sx={{ flexGrow: 3, alignItems: 'flex-end', display: 'flex' }}>
-        <ToggleEditButton onClick={() => setOpen(true)} />
+      <Box sx={{ flexGrow: 4, display: 'flex' }}>
+        <Box sx={{ my: 'auto' }}>
+          {skill.tags.map((t) => (
+            <Chip
+              key={t.id}
+              label={t.name}
+              sx={{ mr: '0.5rem', bgcolor: `${t.color}` }}
+            />
+          ))}
+        </Box>
       </Box>
+      <Box
+        sx={{
+          flexGrow: 3,
+          display: 'flex',
+          justifyContent: 'flex-end',
+          pr: '1rem',
+        }}
+      >
+        <Box sx={{ my: 'auto' }}>
+          <ToggleEditButton onClick={() => setOpen(true)} />
+        </Box>
+      </Box>
+    </>
+  )
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        py: '0.5rem',
+        borderBottom: '1px solid lightgray',
+      }}
+    >
+      {open ? <UpdateSkillForm {...skill} /> : <SkillTableRow />}
     </Box>
   )
-  return open ? <UpdateSkillForm {...skill} /> : <SkillTableRow />
 }
 
 type Props = {
@@ -49,8 +81,12 @@ type Props = {
 }
 
 export const SkillsTable = ({ skills }: Props) => (
-  <Box sx={{ border: '1px solid lightgray', borderRadius: '0.5rem' }}>
-    <Box sx={{ borderBottom: '1px solid lightgray', p: '0.5rem' }}>
+  <Box
+    sx={{ border: '1px solid gray', borderRadius: '0.5rem', borderBottom: 0 }}
+  >
+    <Box
+      sx={{ borderBottom: '1px solid gray', p: '0.5rem', bgcolor: 'lightgrey' }}
+    >
       {skills.length} 件
     </Box>
     <Box>
