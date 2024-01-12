1647f4ce7c381d8e49dff090d7afa19c6b874a47
Yumax-panda <yuma0wall@gmail.com>
2024-01-12 22:38:50 +0900

    ページのパスを変更

--- src/app/(index)/users/[userId]/_components/AddSkillButton/AddSkillButton.tsx
+++ src/app/(index)/users/[userId]/_components/AddSkillButton/AddSkillButton.tsx
@@ -15,7 +15,7 @@ export const AddSkillButton = ({ userId }: Props) => {
   const { data: session } = useSession()
   const router = useRouter()
   const isMyPage = session?.user?.id === userId
-  const onClick = () => router.push(routes.userSkillEdit(userId))
+  const onClick = () => router.push(routes.userSkillEdit())
 
   if (!isMyPage) return null
 
