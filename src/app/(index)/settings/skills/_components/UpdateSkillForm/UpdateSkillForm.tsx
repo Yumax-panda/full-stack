f30b93340096a5c90bcd40cd73b6f0f8b38cf123
Yumax-panda <yuma0wall@gmail.com>
2024-01-13 07:06:39 +0900

    スタイル修正

--- src/app/(index)/settings/skills/_components/UpdateSkillForm/UpdateSkillForm.tsx
+++ src/app/(index)/settings/skills/_components/UpdateSkillForm/UpdateSkillForm.tsx
@@ -2,6 +2,7 @@ import { skills } from '@/constants/skills'
 import { Check, Close } from '@mui/icons-material'
 import {
   Autocomplete,
+  Box,
   IconButton,
   MenuItem,
   OutlinedInput,
@@ -24,13 +25,17 @@ export const UpdateSkillForm = ({ id, name, level, tags, onClose }: Props) => {
   const action = updateSkillAction.bind(null, id)
 
   return (
-    <form action={action} style={{ display: 'flex', width: '100%' }}>
+    <form
+      action={action}
+      style={{ display: 'flex', width: '100%', padding: '1rem' }}
+    >
       <Autocomplete
         options={skills.map((skill) => skill.name)}
         renderInput={(params) => (
           <TextField {...params} label='スキル名' name='name' />
         )}
         defaultValue={skills.find((skill) => skill.name === name)?.name}
+        sx={{ flexGrow: 3, mr: '1rem' }}
       />
       <Slider
         name='level'
@@ -49,7 +54,7 @@ export const UpdateSkillForm = ({ id, name, level, tags, onClose }: Props) => {
         defaultValue={tags.map((tag) => tag.id)}
         input={<OutlinedInput label='タグ' />}
         required
-        sx={{ flexGrow: 1 }}
+        sx={{ flexGrow: 2 }}
       >
         {tags.map((tag) => (
           <MenuItem key={tag.id} value={tag.id}>
@@ -57,17 +62,19 @@ export const UpdateSkillForm = ({ id, name, level, tags, onClose }: Props) => {
           </MenuItem>
         ))}
       </Select>
-      <Tooltip title='保存'>
-        <IconButton type='submit'>
-          <Check />
-        </IconButton>
-      </Tooltip>
+      <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'flex-end' }}>
+        <Tooltip title='保存'>
+          <IconButton type='submit'>
+            <Check />
+          </IconButton>
+        </Tooltip>
 
-      <Tooltip title='閉じる'>
-        <IconButton onClick={onClose}>
-          <Close />
-        </IconButton>
-      </Tooltip>
+        <Tooltip title='閉じる'>
+          <IconButton onClick={onClose}>
+            <Close />
+          </IconButton>
+        </Tooltip>
+      </Box>
     </form>
   )
 }
