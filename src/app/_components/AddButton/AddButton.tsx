cec0a923c0c44a0799938c054072afdeaeabcff8
Yumax-panda <yuma0wall@gmail.com>
2024-01-12 20:07:45 +0900

    ボタンの拡張性を高くするためにオプショナルな引数を増やした

--- src/app/_components/AddButton/AddButton.tsx
+++ src/app/_components/AddButton/AddButton.tsx
@@ -1,19 +1,19 @@
-'use client'
-
 import { Add } from '@mui/icons-material'
 import { IconButton, Tooltip } from '@mui/material'
 
 type Props = {
-  onClick: () => void
+  onClick?: () => void
   text: string
+  type?: 'button' | 'submit' | 'reset'
 }
 
-export const AddButton = ({ onClick, text }: Props) => (
+export const AddButton = ({ onClick, text, type = 'button' }: Props) => (
   <Tooltip title={text}>
     <IconButton
       color='inherit'
       onClick={onClick}
       sx={{ border: '1px solid lightGray' }}
+      type={type}
     >
       <Add sx={{ color: 'GrayText' }} />
     </IconButton>
