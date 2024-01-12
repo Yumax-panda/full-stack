d58a3a7447dc227ccceefabe47246ed0873ad18e
Yumax-panda <yuma0wall@gmail.com>
2024-01-12 19:49:09 +0900

    場所を固定したボタンを追加

---
+++ src/app/(index)/users/[userId]/_components/AddButtonBase/AddButton.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { AddButtonBase } from './'
+
+export default {
+  title: 'UI/AddButtonBase',
+  component: AddButtonBase,
+} as Meta
+
+type Story = StoryObj<typeof AddButtonBase>
+
+export const Default: Story = {
+  args: {
+    text: 'Add',
+    onClick: () => { },
+  },
+}
---
+++ src/app/(index)/users/[userId]/_components/AddButtonBase/AddButtonBase.tsx
@@ -0,0 +1,9 @@
+import { AddButton } from '@/app/_components/AddButton'
+
+type Props = Parameters<typeof AddButton>[0]
+
+export const AddButtonBase = (props: Props) => (
+  <div style={{ position: "fixed", bottom: "2vh", left: "2vw" }}>
+    <AddButton {...props} />
+  </div>
+)
