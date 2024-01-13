1292d746e1eddbc79979c8abd346006fab325c56
Yumax-panda <yuma0wall@gmail.com>
2024-01-13 21:18:19 +0900

    編集ボタンを追加

--- src/app/(index)/works/[workId]/page.tsx
+++ src/app/(index)/works/[workId]/page.tsx
@@ -2,8 +2,10 @@ import { notFound } from 'next/navigation'
 
 import { getSession } from '@/lib/auth'
 import { getWorkById } from '@/repository/work'
+import { Box } from '@mui/material'
 
 import { Content } from './_components/Content'
+import { EditButton } from './_components/EditButton'
 
 export default async function WorkDetailPage({
   params: { workId },
@@ -25,5 +27,21 @@ export default async function WorkDetailPage({
     return notFound()
   }
 
-  return <Content {...work} />
+  return (
+    <>
+      {isMyWork && (
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100%',
+            justifyContent: 'flex-end',
+            my: '0.5rem',
+          }}
+        >
+          <EditButton workId={workId} />
+        </Box>
+      )}
+      <Content {...work} />
+    </>
+  )
 }
