668fb09bc5b9ab35e2c7edb9d63b93af4232547f
Yumax-panda <yuma0wall@gmail.com>
2024-01-14 02:40:24 +0900

    使わないので削除

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -13,7 +13,6 @@ export const env = createEnv({
   },
   server: {
     NEXTAUTH_SECRET: z.string().min(1),
-    NEXTAUTH_URL: z.string().min(1),
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
   },
@@ -31,7 +30,6 @@ export const env = createEnv({
     NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID:
       process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   },
