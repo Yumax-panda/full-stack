e47a922d4a55a10ed0e8652cc9298499a89f1441
Yumax-panda <yuma0wall@gmail.com>
2024-01-13 20:48:08 +0900

    画像がないときのアイコンを追加

--- src/app/(index)/users/[userId]/_components/SkillCard/SkillCard.tsx
+++ src/app/(index)/users/[userId]/_components/SkillCard/SkillCard.tsx
@@ -1,4 +1,5 @@
 import { StarField } from '@/app/(index)/_components/StarField'
+import { QuestionMark } from '@mui/icons-material'
 import {
   Box,
   Card,
@@ -16,51 +17,62 @@ export type Props = Skill & {
   tags: Tag[]
 }
 
-export const SkillCard = ({ name, level, image, tags = [] }: Props) => (
-  <Card sx={{ display: 'flex', flexDirection: 'column', padding: '1rem' }}>
-    <Box sx={{ display: 'flex' }}>
-      <CardMedia
+export const SkillCard = ({ name, level, image, tags = [] }: Props) => {
+  const cardStyle = {
+    width: '6rem',
+    height: '6rem',
+    flexShrink: 0,
+    margin: 'auto',
+  } as const
+
+  return (
+    <Card sx={{ display: 'flex', flexDirection: 'column', padding: '1rem' }}>
+      <Box sx={{ display: 'flex' }}>
+        {image ? (
+          <CardMedia image={image} sx={cardStyle} />
+        ) : (
+          <QuestionMark
+            sx={{
+              ...cardStyle,
+              color: 'lightgray',
+              bgcolor: 'rgba(0, 0, 0, 0.04)',
+              borderRadius: '1.5rem',
+            }}
+          />
+        )}
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography
+            sx={{
+              fontSize: '1.4rem',
+              fontWeight: 'bold',
+              color: 'rgba(0, 0, 0, 0.87)',
+            }}
+          >
+            {name}
+          </Typography>
+          <StarField level={level} />
+        </CardContent>
+      </Box>
+      <Box
         sx={{
-          width: '6rem',
-          height: '6rem',
-          flexShrink: 0,
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: '0.5rem',
           margin: 'auto',
+          height: '2rem',
         }}
-        image={image || undefined}
-        title={name}
-      />
-      <CardContent sx={{ flex: '1 0 auto' }}>
-        <Typography
-          sx={{
-            fontSize: '1.4rem',
-            fontWeight: 'bold',
-            color: 'rgba(0, 0, 0, 0.87)',
-          }}
-        >
-          {name}
-        </Typography>
-        <StarField level={level} />
-      </CardContent>
-    </Box>
-    <Box
-      sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        gap: '0.5rem',
-        margin: 'auto',
-        height: '2rem',
-      }}
-    >
-      {tags.map((tag) => (
-        <Chip
-          key={tag.name}
-          label={tag.name}
-          sx={{
-            backgroundColor: tag.color,
-            color: 'white',
-          }}
-        />
-      ))}
-    </Box>
-  </Card>
-)
+      >
+        {tags.map((tag) => (
+          <Chip
+            key={tag.name}
+            label={tag.name}
+            sx={{
+              backgroundColor: tag.color,
+              color: 'white',
+            }}
+          />
+        ))}
+      </Box>
+    </Card>
+  )
+}
