e7053864d19c73868ec21e8510102f4422c2448b
Yumax-panda <yuma0wall@gmail.com>
2024-01-14 18:25:41 +0900

    フォームの見た目を作った

--- src/app/(index)/settings/profile/page.tsx
+++ src/app/(index)/settings/profile/page.tsx
@@ -1,10 +1,12 @@
 import { notFound } from 'next/navigation'
 
 import { getSession } from '@/lib/auth'
+import { getArticleTokenByUserId } from '@/repository/articleToken'
 import { getUserById } from '@/repository/user'
 import { Box } from '@mui/material'
 
 import { EditProfileForm } from './_components/EditProfileForm'
+import { TokenForm } from './_components/TokenForm'
 
 export default async function Page() {
   const session = await getSession()
@@ -12,10 +14,12 @@ export default async function Page() {
   const user = await getUserById(session.user.id)
 
   if (!user) return notFound()
+  const tokens = await getArticleTokenByUserId(user.id)
 
   return (
     <Box>
       <EditProfileForm {...user} />
+      <TokenForm tokens={tokens} userId={user.id} />
     </Box>
   )
 }
