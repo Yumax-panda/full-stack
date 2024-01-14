4fd2231ecd1b821ab7d1b1124d906e529d65cf00
Yumax-panda <yuma0wall@gmail.com>
2024-01-14 19:16:57 +0900

    プロフィールへ戻るボタンを追加

---
+++ src/app/(index)/works/[workId]/_components/BackButton/BackButton.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import { routes } from '@/lib/routes'
+import { ArrowBackOutlined } from '@mui/icons-material'
+import { IconButton, Tooltip } from '@mui/material'
+
+type Props = {
+  userId: string
+}
+
+export const BackButton = ({ userId }: Props) => {
+  const router = useRouter()
+  const back = () => router.push(routes.userSkill(userId))
+
+  return (
+    <Tooltip
+      title='プロフィールへ戻る'
+      arrow
+      placement='top-end'
+      sx={{
+        border: '1px solid',
+      }}
+    >
+      <IconButton onClick={back}>
+        <ArrowBackOutlined />
+      </IconButton>
+    </Tooltip>
+  )
+}
