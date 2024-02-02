622249a7322fbdf29fb4417f7adc456b01dc39e6
Yumax-panda <yuma0wall@gmail.com>
2024-02-03 05:59:09 +0900

    バリデーションを実装

--- src/models/tag.ts
+++ src/models/tag.ts
@@ -1,5 +1,16 @@
-export type Tag = {
-  id: string
-  name: string
-  color: string
-}
+import { z } from 'zod'
+
+export const createTagSchema = z.object({
+  name: z
+    .string()
+    .min(1)
+    .max(10)
+    .refine((v) => v.trim().length > 0),
+  brief: z
+    .string()
+    .nullable()
+    .transform((v) => v?.trim() ?? v),
+  color: z.string().min(7).max(7),
+})
+
+export type CreateTag = z.infer<typeof createTagSchema>
