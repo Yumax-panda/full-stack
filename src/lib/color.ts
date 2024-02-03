2fb0aeff437a16b132d8e79fadf7701b3feff60c
Yumax-panda <yuma0wall@gmail.com>
2024-02-03 19:49:53 +0900

    RGB変換のユーティリティ関数を定義

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -6,3 +6,11 @@ export function generateRandomColor() {
   }
   return color
 }
+
+export function hexToRgb(hex: string) {
+  const bigint = parseInt(hex.slice(1), 16)
+  const r = (bigint >> 16) & 255
+  const g = (bigint >> 8) & 255
+  const b = bigint & 255
+  return { r, g, b }
+}
