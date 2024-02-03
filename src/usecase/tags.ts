6d1b1beace6e3dc21de557a0e5f23c6b877a527c
Yumax-panda <yuma0wall@gmail.com>
2024-02-04 02:46:08 +0900

    DB操作のロジックを実装

--- src/usecase/tags.ts
+++ src/usecase/tags.ts
@@ -1,11 +1,23 @@
 import 'server-only'
 
-import { createTag, getTagsByUserId } from '@/repository/tag'
-import type { CreateTag } from '@/models'
+import {
+  createTag,
+  getTagsByUserId,
+  updateTag as defaultUpdateTag,
+} from '@/repository/tag'
+import type { CreateTag, UpdateTag } from '@/models'
 
 async function isDuplicateTag(userId: string, name: string) {
   const tags = await getTagsByUserId(userId)
   return tags.some((tag) => tag.name === name)
+}
+
+async function willBeDuplicateTag(userId: string, tagId: string, name: string) {
+  const tags = await getTagsByUserId(userId)
+  if (!tags.some((tag) => tag.id === tagId)) {
+    throw new Error('タグが存在しません')
+  }
+  return tags.some((tag) => tag.name === name && tag.id !== tagId)
 }
 
 export async function createNewTag(data: CreateTag & { userId: string }) {
@@ -14,4 +26,15 @@ export async function createNewTag(data: CreateTag & { userId: string }) {
     throw new Error('タグ名が重複しています')
   }
   return createTag(data)
+}
+
+export async function updateTag({
+  id,
+  userId,
+  ...data
+}: UpdateTag & { userId: string }) {
+  if (await willBeDuplicateTag(userId, id, data.name)) {
+    throw new Error('タグ名が重複しています')
+  }
+  return defaultUpdateTag({ id, userId, ...data })
 }
