b6b9b1b56efd781582e647c89f7aaf1caed73955
Yumax-panda <yuma0wall@gmail.com>
2024-02-03 16:30:57 +0900

    フォーマット

--- src/app/(index)/settings/tags/_components/ColorPallet/ColorPallet.tsx
+++ src/app/(index)/settings/tags/_components/ColorPallet/ColorPallet.tsx
@@ -1,5 +1,5 @@
 // TODO: メニューみたいに外側をクリックすると閉じるようにする
-import { Paper, Typography } from "@mui/material"
+import { Paper, Typography } from '@mui/material'
 
 type ColorPalletProps = {
   onSelect: (color: string) => void
@@ -21,40 +21,44 @@ const ColorCell = ({ color, onClick }: ColorCellProps) => (
       border: 'none',
       cursor: 'pointer',
     }}
-    type="button"
+    type='button'
     onClick={onClick}
   />
 )
 
 export const ColorPallet = ({ onSelect }: ColorPalletProps) => {
   const colors = [
-    "#B60205",
-    "#D93F0B",
-    "#FBCA04",
-    "#0E8A16",
-    "#006B75",
-    "#1D76DB",
-    "#0052CC",
-    "#5319E7",
-    "#E99695",
-    "#F9D0C4",
-    "#FEF2C0",
-    "#C2E0C6",
-    "#BFDADC",
-    "#C5DEF5",
-    "#BFD4F2",
-    "#D4C5F9",
+    '#B60205',
+    '#D93F0B',
+    '#FBCA04',
+    '#0E8A16',
+    '#006B75',
+    '#1D76DB',
+    '#0052CC',
+    '#5319E7',
+    '#E99695',
+    '#F9D0C4',
+    '#FEF2C0',
+    '#C2E0C6',
+    '#BFDADC',
+    '#C5DEF5',
+    '#BFD4F2',
+    '#D4C5F9',
   ]
   const onClick = (color: string) => () => onSelect(color)
   return (
-
-    <Paper sx={{ width: "fit-content", padding: "10px" }}>
-      <Typography color="GrayText" sx={{ fontSize: "14px" }}>Choose from default colors:</Typography>
-      <div className='color-pallet' style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(8, 24px)',
-        gap: '4px',
-      }}>
+    <Paper sx={{ width: 'fit-content', padding: '10px' }}>
+      <Typography color='GrayText' sx={{ fontSize: '14px' }}>
+        Choose from default colors:
+      </Typography>
+      <div
+        className='color-pallet'
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(8, 24px)',
+          gap: '4px',
+        }}
+      >
         {colors.map((color) => (
           <ColorCell key={color} color={color} onClick={onClick(color)} />
         ))}
