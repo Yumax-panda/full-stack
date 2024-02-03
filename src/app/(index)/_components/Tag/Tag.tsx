5bff0ad31ab5cfdd42720e9f4ce03ec500113072
Yumax-panda <yuma0wall@gmail.com>
2024-02-03 19:46:45 +0900

    透明度をいじって良い感じに

--- src/app/(index)/_components/Tag/Tag.tsx
+++ src/app/(index)/_components/Tag/Tag.tsx
@@ -1,14 +1,18 @@
 import { Chip, Tooltip } from '@mui/material'
+import { hexToRgb } from '@/lib/color'
 import type { Tag as TagType } from '@prisma/client'
 
 type Props = Pick<TagType, 'name' | 'color' | 'brief'>
 
 export const Tag = ({ name, color, brief }: Props) => {
+  const bgColorRGB = hexToRgb(color)
+  const bgcolor = `rgba(${bgColorRGB.r}, ${bgColorRGB.g}, ${bgColorRGB.b}, 0.18)`
+
   return (
     <Tooltip title={brief} placement='bottom'>
       <Chip
         label={name}
-        style={{ backgroundColor: color, color: 'white' }}
+        sx={{ bgcolor, color, fontWeight: 'bold', borderColor: color }}
         variant='outlined'
       />
     </Tooltip>
