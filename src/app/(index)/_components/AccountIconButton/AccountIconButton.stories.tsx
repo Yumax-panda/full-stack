117befd912c5fdd4ac565c10e9e7a5336fc574d1
Yumax-panda <yuma0wall@gmail.com>
2024-02-04 19:08:21 +0900

    typoを修正

--- src/app/(index)/_components/AccountIconButton/AccountIconButton.stories.tsx
+++ src/app/(index)/_components/AccountIconButton/AccountIconButton.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { AccountIconButton } from './'
+import { AccountIconButton } from '.'
 
 export default {
   title: 'UI/AccountIconButton',
