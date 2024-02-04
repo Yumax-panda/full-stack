e189252b2775e5f937ac97084702e7524c507f95
Yumax-panda <yuma0wall@gmail.com>
2024-02-05 02:13:02 +0900

    編集のロジック追加

---
+++ src/app/(index)/settings/skills/_components/hooks/useUpdateSkillForm.ts
@@ -0,0 +1,96 @@
+import { useForm } from 'react-hook-form'
+import type { UseFormReturn } from 'react-hook-form'
+import { useState, useCallback, type FormEventHandler } from 'react'
+import { useRouter } from 'next/navigation'
+import { createSkillSchema } from '@/models'
+import type { CreateSkillProps } from '@/models'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useToastPromise } from '@/app/_components/hooks/useToastPromise'
+import { getImage } from '@/constants/skills'
+import type { SkillWithTags } from '@/models'
+
+type Props = {
+  onClose: () => void
+  skill: SkillWithTags
+}
+
+type FormValues = Omit<CreateSkillProps, 'image'>
+
+export type UseCreateSkillFormReturn = Pick<
+  UseFormReturn<FormValues>,
+  'register' | 'formState'
+> & {
+  handleSubmit: FormEventHandler<HTMLFormElement>
+  isLoading: boolean
+  handleChangeTagIds: (id: string | string[]) => void
+  selectedTags: string[]
+}
+
+export const useCreateSkillForm = ({
+  onClose,
+  skill,
+}: Props): UseCreateSkillFormReturn => {
+  const {
+    register,
+    handleSubmit: defaultHandleSubmit,
+    formState,
+    setValue,
+    watch,
+  } = useForm<FormValues>({
+    defaultValues: {
+      name: skill.name,
+      level: skill.level,
+      tags: skill.tags.map((tag) => tag.id),
+    },
+    resolver: zodResolver(createSkillSchema),
+  })
+  const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter()
+  const { task } = useToastPromise({
+    pending: 'スキルを更新中...',
+    success: 'スキルを更新しました',
+    action: async (data: FormValues) => {
+      await updateSkill({
+        ...data,
+        image: getImage(data.name),
+      })
+      router.refresh()
+      onClose()
+    },
+    setIsLoading,
+  })
+
+  const updateSkill = useCallback(
+    async (data: CreateSkillProps) => {
+      const apiUrl = `/api/skills/${skill.id}`
+      const res = await fetch(apiUrl, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      if (!res.ok) {
+        const error = await res.json()
+        throw new Error(error.error)
+      }
+      return
+    },
+    [skill.id],
+  )
+
+  const handleChangeTagIds = (value: string | string[]) => {
+    setValue('tags', typeof value === 'string' ? value.split(',') : value)
+  }
+
+  const selectedTags = watch('tags')
+
+  return {
+    register,
+    handleSubmit: defaultHandleSubmit(task),
+    formState,
+    isLoading,
+    handleChangeTagIds,
+    selectedTags,
+  }
+}
