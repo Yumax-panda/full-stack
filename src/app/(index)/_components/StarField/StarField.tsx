ab552d10e702c10e45c3c77d5790a0231035e687
Yumax-panda <yuma0wall@gmail.com>
2024-02-09 22:40:17 +0900

    muiのコンポーネント使って簡単に

--- src/app/(index)/_components/StarField/StarField.tsx
+++ src/app/(index)/_components/StarField/StarField.tsx
@@ -1,30 +1,10 @@
-import { Star as MuiStar, StarOutline } from '@mui/icons-material'
-import { Box } from '@mui/material'
+import { Rating } from '@mui/material'
 
 type Props = {
   level: number
-  size?: string | number
+  size?: 'small' | 'medium' | 'large'
 }
 
-type StarProps = {
-  filled: boolean
-  size?: string | number
-}
-
-const Star = ({ filled, size = '2.5rem' }: StarProps) => {
-  const Icon = filled ? MuiStar : StarOutline
-  const color = filled ? 'gold' : 'gray'
-  return <Icon sx={{ color, height: size, width: size }} />
-}
-
-export const StarField = ({ level, size }: Props) => {
-  const filledStarNum = Math.min(3, Math.max(0, level))
-  const stars = Array.from({ length: 3 }, (_, i) => i < filledStarNum)
-  return (
-    <Box sx={{ display: 'flex' }}>
-      {stars.map((filled, i) => (
-        <Star key={i} filled={filled} size={size} />
-      ))}
-    </Box>
-  )
-}
+export const StarField = ({ level, size }: Props) => (
+  <Rating name='rating' value={level} max={3} size={size} readOnly />
+)
