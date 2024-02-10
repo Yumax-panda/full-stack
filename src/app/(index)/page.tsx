a2c6524bf6b9c16e3c32294d02cc22120d232d8d
Yumax-panda <yuma0wall@gmail.com>
2024-02-11 07:22:45 +0900

    文言を入れた

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           },
           display: 'flex',
           justifyContent: 'center',
-          my: "0.5rem"
+          my: '0.5rem',
         }}
       >
         <div>全てのエンジニアのための</div>
@@ -27,6 +27,16 @@ export default function Home() {
       </Typography>
       <SignInButton />
       <FeatureSection />
+      <Typography
+        variant='body1'
+        sx={{ mt: '1rem', textAlign: 'left', lineHeight: 2 }}
+      >
+        エンジニアは技術があることも大切ですが、それを活かしアピールする機会がないと宝の持ち腐れです。
+        自分のスキルや経験をアピールする手段の一つとしてポートフォリオがあります。
+        しかしポートフォリオは作成するための手間や時間がかかるため、作ることを諦めてしまう方も多いのではないでしょうか。
+        このサービスは皆さんが手軽にポートフォリオを作って共有できることを目指しています。
+        皆さんが今までに培ってきたスキルや達成したこと、挑戦していることなんでも共有しましょう。
+      </Typography>
     </Box>
   )
 }
