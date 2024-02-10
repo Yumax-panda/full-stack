1dd4fa3a8b765474e41f93a6637018a686ecf300
Yumax-panda <yuma0wall@gmail.com>
2024-02-11 06:32:43 +0900

    極力デフォルトTyphographyを使う

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,22 +1,22 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
-import { Subtitle } from '../_components/Text/Subtitle/Subtitle'
-import { Title } from '../_components/Text/Title'
-import { Feature } from './_components/Feature'
 import { SignInButton } from './_components/SignInButton'
+import { FeatureSection } from './_components/FeatureSection'
 
 export default function Home() {
   return (
     <Box>
       <Box sx={{ margin: 'auto', textAlign: 'center' }}>
-        <Title title='Full Stack' />
-        <Subtitle
-          text='エンジニアのためのポートフォリオサイト'
-          sx={{ my: '0.5rem' }}
-        />
+        <Typography variant='h4' sx={{ fontWeight: 'bold' }}>
+          Full Stack
+        </Typography>
+        <Typography variant='subtitle1' sx={{ fontWeight: 'bold' }}>
+          全てのエンジニアのためのポートフォリオ作成サービス
+        </Typography>
         <SignInButton />
+        <Typography variant='caption'>About</Typography>
+        <FeatureSection />
       </Box>
-      <Feature />
     </Box>
   )
 }
