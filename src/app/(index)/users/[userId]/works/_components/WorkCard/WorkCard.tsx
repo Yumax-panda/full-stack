ba14ab63cf41f644e9ec1e6a493446c25c8214e1
Yumax-panda <yuma0wall@gmail.com>
2024-02-12 01:39:29 +0900

    境界をハッキリさせる

--- src/app/(index)/users/[userId]/works/_components/WorkCard/WorkCard.tsx
+++ src/app/(index)/users/[userId]/works/_components/WorkCard/WorkCard.tsx
@@ -72,6 +72,7 @@ export const WorkCard = ({
             width={368}
             height={230}
             component='img'
+            sx={{ borderBottom: '1px solid lightgray' }}
           />
         </Box>
       ) : (
