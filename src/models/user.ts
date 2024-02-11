c2bd77c9a25b5e066edd49c1744392b195e51f0c
Yumax-panda <yuma0wall@gmail.com>
2024-02-11 18:01:01 +0900

    zodのエラーを日本語へ

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,6 +2,11 @@ import { z } from 'zod'
 
 export const updateUserSchema = z.object({
   id: z.string().refine(Boolean),
+  name: z
+    .string()
+    .min(1, '1文字以上で名前を入力してください')
+    .max(30, '名前は30文字までです')
+    .transform((v) => v.trim()),
   location: z.string().nullable(),
   organization: z.string().nullable(),
 })
