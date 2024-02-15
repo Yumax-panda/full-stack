206c393b4790ba75ad99d6ab2bfa9eafaef22bfa
Yumax-panda <yuma0wall@gmail.com>
2024-02-15 22:16:58 +0900

    Prismaのエラーで検知させるように修正

--- src/app/api/skills/route.ts
+++ src/app/api/skills/route.ts
@@ -1,10 +1,11 @@
 import { tag } from '@/lib/routes'
 import { revalidateTag } from 'next/cache'
 import { getSession } from '@/lib/auth'
-import { createSkill } from '@/usecase/skills'
+import { createSkill } from '@/repository/skill'
 import { NextRequest, NextResponse } from 'next/server'
 import { message } from '@/lib/message'
 import { createSkillSchema } from '@/models'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 
 export async function POST(req: NextRequest) {
   const session = await getSession()
@@ -17,7 +18,14 @@ export async function POST(req: NextRequest) {
     const skillPayload = createSkillSchema.parse(body)
     await createSkill({ ...skillPayload, userId: session.user.id })
   } catch (e) {
+    if (e instanceof PrismaClientKnownRequestError && e.code === 'P2002') {
+      return NextResponse.json(
+        { error: '同じスキル名は登録できません' },
+        { status: 400 },
+      )
+    }
     if (e instanceof Error) {
+      console.error('failed to create skill', e)
       return NextResponse.json({ error: e.message }, { status: 400 })
     }
     console.error('failed to create skill', e)
--- src/usecase/skills.ts
+++ src/usecase/skills.ts
@@ -1,34 +1,8 @@
 import 'server-only'
 
 import { prisma } from '@/lib/client'
-import { createSkill as create } from '@/repository/skill'
 
-import type { UpdateSkillProps, CreateSkillProps } from '@/models'
-
-type IsSkillNameDuplicateProps = {
-  userId: string
-  skillId?: string
-  newName: string
-}
-
-export async function isSkillNameDuplicate({
-  userId,
-  skillId,
-  newName,
-}: IsSkillNameDuplicateProps) {
-  const skills = await prisma.skill.findMany({
-    where: {
-      userId,
-      name: newName,
-    },
-  })
-
-  if (skillId) {
-    return skills.some((skill) => skill.id !== skillId)
-  }
-
-  return Boolean(skills.length)
-}
+import type { UpdateSkillProps } from '@/models'
 
 export async function updateSkillWithTagIds({
   id,
@@ -36,16 +10,6 @@ export async function updateSkillWithTagIds({
   tags,
   ...props
 }: UpdateSkillProps & { userId: string }) {
-  const isNameDuplicating = await isSkillNameDuplicate({
-    userId,
-    skillId: id,
-    newName: props.name,
-  })
-
-  if (isNameDuplicating) {
-    throw new Error('スキル名が重複しています')
-  }
-
   await prisma.skill.update({
     where: {
       id,
@@ -65,21 +29,6 @@ export async function updateSkillWithTagIds({
       tagId,
     })),
   })
-}
-
-export async function createSkill(
-  props: CreateSkillProps & { userId: string },
-) {
-  const isNameDuplicating = await isSkillNameDuplicate({
-    userId: props.userId,
-    newName: props.name,
-  })
-
-  if (isNameDuplicating) {
-    throw new Error('スキル名が重複しています')
-  }
-
-  return create(props)
 }
 
 export async function deleteSkill(skillId: string) {
