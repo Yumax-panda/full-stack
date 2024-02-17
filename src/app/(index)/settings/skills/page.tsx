9564019d64a00dcee721e130d9d88b09699181e1
Yumax-panda <yuma0wall@gmail.com>
2024-02-17 21:21:27 +0900

    個別ページにメタデータを付与

--- src/app/(index)/settings/skills/page.tsx
+++ src/app/(index)/settings/skills/page.tsx
@@ -1,11 +1,20 @@
+import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
-
+import { env } from '@/lib/env.mjs'
 import { getSession } from '@/lib/auth'
 import { getSkillsWithTagsByUserId } from '@/repository/skill'
 import { getTagsByUserId } from '@/repository/tag'
 
 import { TopContent } from '../_components/TopContent'
 import { SkillsTable } from './_components/SkillsTable'
+
+export const metadata: Metadata = {
+  title: 'スキルを編集',
+  metadataBase: new URL(env.NEXTAUTH_URL),
+  openGraph: {
+    title: 'スキルを編集',
+  },
+}
 
 export default async function Page() {
   const session = await getSession()
