9e3552095aea7d6b65ac99b209648bba321865f1
Yumax-panda <yuma0wall@gmail.com>
2024-02-18 15:44:12 +0900

    パンくずリストを設置

--- src/app/(index)/settings/profile/page.tsx
+++ src/app/(index)/settings/profile/page.tsx
@@ -5,10 +5,11 @@ import { getSession } from '@/lib/auth'
 import { getArticleTokenByUserId } from '@/repository/articleToken'
 import { getUserById } from '@/repository/user'
 import { Box } from '@mui/material'
-
+import { Breadcrumbs } from '@/app/_components/Breadcrumbs'
 import { TopContent } from '../_components/TopContent'
 import { EditProfileForm } from './_components/EditProfileForm'
 import { TokenForm } from './_components/TokenForm'
+import { routes } from '@/lib/routes'
 
 export const metadata: Metadata = {
   title: 'プロフィールを編集',
@@ -29,6 +30,15 @@ export default async function Page() {
   return (
     <Box>
       <TopContent userId={user.id} />
+      <Box sx={{ my: '1.5rem' }}>
+        <Breadcrumbs
+          links={[
+            // 構造上はuserSkillを使わないが、プロフィールへ戻る導線として入れている
+            { href: routes.userSkill(user.id), label: user.name || '無名' },
+            { href: routes.userProfileSettings(), label: 'プロフィールを編集' },
+          ]}
+        />
+      </Box>
       <EditProfileForm {...user} />
       <TokenForm tokens={tokens} userId={user.id} />
     </Box>
