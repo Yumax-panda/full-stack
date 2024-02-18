7a9ab99cbd39eb2cbbeb31b546b8a859a7ff263b
Yumax-panda <yuma0wall@gmail.com>
2024-02-18 17:57:45 +0900

    textではなくvarcharにした

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,8 +7,8 @@ export const updateUserSchema = z.object({
     .min(1, '1文字以上で名前を入力してください')
     .max(30, '名前は30文字までです')
     .transform((v) => v.trim()),
-  location: z.string().nullable(),
-  organization: z.string().nullable(),
+  location: z.string().max(160, '居住地は100文字までです').nullable(),
+  organization: z.string().max(160, '所属は100文字までです').nullable(),
   bio: z.string().max(160, '自己紹介は100文字までです').nullable(),
 })
 
