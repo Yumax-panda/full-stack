aadde37a8cb54d8df2e00841f7fa388f5b49b0de
Yumax-panda <yuma0wall@gmail.com>
2024-02-19 04:30:33 +0900

    一旦コミット

--- src/app/(index)/works/[workId]/_components/Content/Content.tsx
+++ src/app/(index)/works/[workId]/_components/Content/Content.tsx
@@ -19,11 +19,13 @@ export const Content = ({ title, content, thumbnail, user }: Props) => (
     <Box sx={{ display: 'grid', gridTemplateColumns: '1fr auto' }}>
       <EditorContainer sx={{ m: 0 }}>
         <Title title={title || '無題'} />
-        <Thumbnail url={thumbnail} onClick={() => {}} omitDeleteButton />
+        <Thumbnail url={thumbnail} onClick={() => { }} omitDeleteButton />
         <Tiptap content={content || ''} editable={false} />
       </EditorContainer>
       <div style={{ width: '300px' }}>
         <UserCard user={user} />
+        {/* TODO: 目次を追加 */}
+        <Box sx={{ position: 'sticky', top: '2rem', mt: '2rem' }}>test</Box>
       </div>
     </Box>
     <Box sx={{ borderTop: '1px solid lightgray', pt: '1rem' }}>
