17875cb387ee22b46d257aa447db35831448099d
Yumax-panda <yuma0wall@gmail.com>
2024-02-18 21:51:57 +0900

    userプロパティも含めるように

--- src/app/(index)/works/[workId]/_components/Content/Content.tsx
+++ src/app/(index)/works/[workId]/_components/Content/Content.tsx
@@ -1,14 +1,21 @@
 'use client'
 
-import type { Work as Props } from '@prisma/client'
+import type { Work } from '@prisma/client'
 import { Title } from '@/app/_components/Text/Title'
 import { Thumbnail } from '@/app/_components/Thumbnail'
 import { Tiptap } from '@/app/_components/Tiptap'
+import { UserSummary } from '../UserSummary'
+import type { ComponentProps } from 'react'
 
-export const Content = ({ title, content, thumbnail }: Props) => (
+type Props = {
+  user: ComponentProps<typeof UserSummary>['user']
+} & Pick<Work, 'title' | 'content' | 'thumbnail'>
+
+export const Content = ({ title, content, thumbnail, user }: Props) => (
   <div>
     <Title title={title || '無題'} />
     <Thumbnail url={thumbnail} onClick={() => {}} omitDeleteButton />
     <Tiptap content={content || ''} editable={false} />
+    <UserSummary user={user} />
   </div>
 )
