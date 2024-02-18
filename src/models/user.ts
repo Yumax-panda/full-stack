6575d4e7ce20df19102a6d779f7a42fc4df0a31c
Yumax-panda <yuma0wall@gmail.com>
2024-02-18 17:58:57 +0900

    100文字に修正

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,9 +7,9 @@ export const updateUserSchema = z.object({
     .min(1, '1文字以上で名前を入力してください')
     .max(30, '名前は30文字までです')
     .transform((v) => v.trim()),
-  location: z.string().max(160, '居住地は100文字までです').nullable(),
-  organization: z.string().max(160, '所属は100文字までです').nullable(),
-  bio: z.string().max(160, '自己紹介は100文字までです').nullable(),
+  location: z.string().max(100, '居住地は100文字までです').nullable(),
+  organization: z.string().max(100, '所属は100文字までです').nullable(),
+  bio: z.string().max(100, '自己紹介は100文字までです').nullable(),
 })
 
 export type UpdateUserProps = z.infer<typeof updateUserSchema>
