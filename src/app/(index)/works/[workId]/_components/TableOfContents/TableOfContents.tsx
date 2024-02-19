5a359073ddaf0f2b2c7bf1d4523f4f1767e2107e
Yumax-panda <yuma0wall@gmail.com>
2024-02-19 14:31:34 +0900

    不要なインポートを削除

--- src/app/(index)/works/[workId]/_components/TableOfContents/TableOfContents.tsx
+++ src/app/(index)/works/[workId]/_components/TableOfContents/TableOfContents.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import { CardContainer } from '../CardContainer'
 import styles from './TableOfContents.module.css'
 
