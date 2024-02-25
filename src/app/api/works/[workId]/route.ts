7767abda1ccbc1accaebe249fb7b7c72045a4af9
Yumax-panda <yuma0wall@gmail.com>
2024-02-25 21:30:56 +0900

    削除用のAPIを追加

--- src/app/api/works/[workId]/route.ts
+++ src/app/api/works/[workId]/route.ts
@@ -5,7 +5,7 @@ import { getSession } from '@/lib/auth'
 import { message } from '@/lib/message'
 import { tag } from '@/lib/routes'
 import { updateWorkInServer } from '@/models'
-import { updateWork } from '@/usecase/work'
+import { updateWork, deleteWork } from '@/usecase/work'
 
 // FIXME: workIdが冗長なので削除して良い
 export async function PATCH(
@@ -28,6 +28,26 @@ export async function PATCH(
       return NextResponse.json({ error: e.message }, { status: 400 })
     }
     console.error('failed to update work', e)
+    return NextResponse.json({ error: message.unknown }, { status: 500 })
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params: { workId } }: { params: { workId: string } },
+) {
+  const session = await getSession()
+  if (!session) {
+    return NextResponse.json({ error: message.unauthorized }, { status: 401 })
+  }
+  try {
+    await deleteWork(workId)
+    revalidateTag(tag.work)
+  } catch (e) {
+    if (e instanceof Error) {
+      return NextResponse.json({ error: e.message }, { status: 400 })
+    }
+    console.error('failed to delete work', e)
     return NextResponse.json({ error: message.unknown }, { status: 500 })
   }
 }
