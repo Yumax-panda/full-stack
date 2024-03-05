43aa49da6bdb3e463b6737b7f541233f3838d327
Yumax-panda <yuma0wall@gmail.com>
2024-03-06 02:37:02 +0900

    リンク埋め込みを取得するように実装

--- src/app/_components/hooks/useEditorMenu.ts
+++ src/app/_components/hooks/useEditorMenu.ts
@@ -2,8 +2,6 @@ import { useCallback } from 'react'
 
 import type { Editor } from '@tiptap/react'
 
-import { fetchSiteData } from '@/lib/ogp'
-
 type Props = {
   editor: Editor | null
 }
@@ -17,8 +15,14 @@ export const useEditorMenu = ({ editor }: Props): UseEditorMenuReturn => {
   const onLinkEmbedAdd = async () => {
     const url = window.prompt('URLを入力してください')
     if (!url) return
-
-    const ogpData = await fetchSiteData(url)
+    const encodedUrl = encodeURIComponent(url)
+    const apiUrl = `/api/embed?url=${encodedUrl}`
+    const ogpData = await fetch(apiUrl).then((res) => res.json())
+    const { error } = ogpData
+    if (error) {
+      window.alert('OGPデータの取得に失敗しました')
+      return
+    }
     // FIXME: ここにOGPを埋め込む処理を書く
     // ref: https://github.com/ueberdosis/tiptap/blob/main/packages/extension-youtube/src/youtube.ts
     editor
--- src/lib/ogp.ts
+++ src/lib/ogp.ts
@@ -15,34 +15,3 @@ export async function fetchOgp(url: string): Promise<string | undefined> {
   }
   return undefined
 }
-
-type SiteData = {
-  title?: string
-  image?: string
-  favicon?: string
-  siteName?: string
-  url: string
-}
-
-export async function fetchSiteData(url: string): Promise<SiteData> {
-  const response = await fetch(url, {
-    headers: {
-      'Allow-Cross-Origin-Origin': '*',
-    },
-    next: { revalidate: 259200 }, // 1month
-  })
-  const text = await response.text()
-  const doc = parse(text)
-  const title = doc.querySelector('meta[property="og:title"]')
-  const image = doc.querySelector('meta[property="og:image"]')
-  const favicon = doc.querySelector('link[rel="icon"]')
-  const siteName = doc.querySelector('meta[property="og:site_name"]')
-
-  return {
-    image: image?.getAttribute('content'),
-    favicon: favicon?.getAttribute('href'),
-    siteName: siteName?.getAttribute('content'),
-    title: title?.getAttribute('content'),
-    url,
-  }
-}
