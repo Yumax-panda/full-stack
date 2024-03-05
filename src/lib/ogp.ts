a123cf5c86a734f8350faa16c82e8981267e1850
Yumax-panda <yuma0wall@gmail.com>
2024-03-05 18:34:54 +0900

    今はダミー

--- src/lib/ogp.ts
+++ src/lib/ogp.ts
@@ -51,8 +51,8 @@ export async function fetchSiteData(url: string): Promise<SiteData> {
       'https://qiita-user-contents.imgix.net/https%3A%2F%2Fcdn.qiita.com%2Fassets%2Fpublic%2Farticle-ogp-background-9f5428127621718a910c8b63951390ad.png?ixlib=rb-4.0.0&w=1200&mark64=aHR0cHM6Ly9xaWl0YS11c2VyLWNvbnRlbnRzLmltZ2l4Lm5ldC9-dGV4dD9peGxpYj1yYi00LjAuMCZ3PTkxNiZoPTMzNiZ0eHQ9JUUzJTgwJTkwUmVhY3QlRTMlODAlOTElRTMlODIlQTQlRTMlODMlOTklRTMlODMlQjMlRTMlODMlODglRTMlODMlOEYlRTMlODMlQjMlRTMlODMlODklRTMlODMlQTklRTMlODMlOTclRTMlODMlQUQlRTMlODMlOTElRTMlODMlODYlRTMlODIlQTMlRTQlQjglODAlRTglQTYlQTclRTMlODAlOTBKYXZhU2NyaXB0JUUzJTgwJTkxJnR4dC1jb2xvcj0lMjMyMTIxMjEmdHh0LWZvbnQ9SGlyYWdpbm8lMjBTYW5zJTIwVzYmdHh0LXNpemU9NTYmdHh0LWNsaXA9ZWxsaXBzaXMmdHh0LWFsaWduPWxlZnQlMkN0b3Amcz1jZTYwN2JlNmZmZWIzZGI0OTU1ODFmMGYzYjhmYzE0Nw&mark-x=142&mark-y=112&blend64=aHR0cHM6Ly9xaWl0YS11c2VyLWNvbnRlbnRzLmltZ2l4Lm5ldC9-dGV4dD9peGxpYj1yYi00LjAuMCZ3PTcxNiZ0eHQ9JTQwUC1tYW5fQnJvd24mdHh0LWNvbG9yPSUyMzIxMjEyMSZ0eHQtZm9udD1IaXJhZ2lubyUyMFNhbnMlMjBXNiZ0eHQtc2l6ZT0zMiZ0eHQtYWxpZ249bGVmdCUyQ3RvcCZzPTExNGY3YTU2NmYxYWMwMTU2Nzg1MDQ5MzhmMDhmNmY1&blend-x=142&blend-y=491&blend-mode=normal&s=faf6d0e6d9a7eaa51a2d7eae4eee0abd',
     favicon:
       'https://cdn.qiita.com/assets/favicons/public/production-c620d3e403342b1022967ba5e3db1aaa.ico',
-    siteName: 'Qiita',
-    title: 'Qiita',
+    siteName: 'sitename_Qiita',
+    title: 'title_Qiita',
     url,
   }
 }
