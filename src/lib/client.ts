cbebc9f70c00cba29740351fd1d36c6948a068fb
Yumax-panda <yuma0wall@gmail.com>
2024-03-28 01:03:17 +0900

    planetscaleのアダプタを削除

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,30 +1,15 @@
-import { Client } from '@planetscale/database'
-import { PrismaPlanetScale } from '@prisma/adapter-planetscale'
 import { PrismaClient } from '@prisma/client'
-import { fetch as undiciFetch } from 'undici'
-
-import { env } from './env.mjs'
-
-const generatePrismaClient = (): PrismaClient => {
-  const client = new Client({
-    url: env.DATABASE_URL,
-    fetch: undiciFetch,
-  })
-
-  const adapter = new PrismaPlanetScale(client)
-  return new PrismaClient({ adapter })
-}
 
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = generatePrismaClient()
+  prisma = new PrismaClient()
 } else {
   const globalWithPrisma = global as typeof globalThis & {
     prisma: PrismaClient
   }
   if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = generatePrismaClient()
+    globalWithPrisma.prisma = new PrismaClient()
   }
   prisma = globalWithPrisma.prisma
 }
