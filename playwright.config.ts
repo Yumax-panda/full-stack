01605c338d851e3969ef57c5e486b335cbc5de3c
Yumax-panda <yuma0wall@gmail.com>
2024-03-29 03:32:02 +0900

    不要なコメントを削除

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,8 +33,9 @@ export default defineConfig({
   },
 
   webServer: {
-    command: 'npm run build && npm run start',
+    command: 'npm run dev',
     url: baseURL,
+    reuseExistingServer: !process.env.CI,
   },
 
   /* Configure projects for major browsers */
@@ -53,32 +54,5 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 })
