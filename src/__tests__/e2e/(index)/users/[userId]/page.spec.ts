855c8e906efc06e824c3002582434eb428f13d94
Yumax-panda <yuma0wall@gmail.com>
2024-03-31 02:47:05 +0900

    マイページのe2eテスト追加

---
+++ src/__tests__/e2e/(index)/users/[userId]/page.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test'
+
+import { authenticatedUserCreatedTest } from '@/__tests__/e2e/utils/auth'
+import { userCreatedTest } from '@/__tests__/utils/factory'
+import { routes } from '@/lib/routes'
+
+test('存在しないユーザーのページ', async ({ page }) => {
+  await page.goto(routes.userSkill('not-exist'), {
+    waitUntil: 'domcontentloaded',
+  })
+  await expect(page).toHaveTitle(/.*404/)
+})
+
+test('存在するユーザーページ', async ({ page }) => {
+  userCreatedTest(async ({ user }) => {
+    await page.goto(routes.userSkill(user.id), {
+      waitUntil: 'domcontentloaded',
+    })
+    await expect(page).toHaveTitle(`${user.name} | Full Stack`)
+  })
+})
+
+test('存在するユーザーページ: ログインしている', async ({ browser }) => {
+  authenticatedUserCreatedTest({
+    browser,
+    test: async ({ user, page }) => {
+      await page.goto(routes.userSkill(user.id), {
+        waitUntil: 'domcontentloaded',
+      })
+      await expect(page).toHaveTitle(`${user.name} | Full Stack`)
+      await page.click('text=スキルを追加')
+      await expect(page).toHaveURL(routes.userSkillEdit())
+    },
+  })
+})
