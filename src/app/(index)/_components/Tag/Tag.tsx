49e4e94279caba151dde3f279c19ce91e23489cf
Yumax-panda <yuma0wall@gmail.com>
2024-03-31 21:56:45 +0900

    タグ削除メソッドを追加

--- src/app/(index)/_components/Tag/Tag.tsx
+++ src/app/(index)/_components/Tag/Tag.tsx
@@ -4,9 +4,11 @@ import type { Tag as TagType } from '@prisma/client'
 
 import { hexToRgb } from '@/lib/color'
 
-type Props = Pick<TagType, 'name' | 'color' | 'brief'>
+type Props = Pick<TagType, 'name' | 'color' | 'brief'> & {
+  onDelete?: () => void
+}
 
-export const Tag = ({ name, color, brief }: Props) => {
+export const Tag = ({ name, color, brief, onDelete }: Props) => {
   const bgColorRGB = hexToRgb(color)
   const bgcolor = `rgba(${bgColorRGB.r}, ${bgColorRGB.g}, ${bgColorRGB.b}, 0.18)`
 
@@ -16,6 +18,7 @@ export const Tag = ({ name, color, brief }: Props) => {
         label={name}
         sx={{ bgcolor, color, fontWeight: 'bold', borderColor: color }}
         variant='outlined'
+        onDelete={onDelete}
       />
     </Tooltip>
   )
