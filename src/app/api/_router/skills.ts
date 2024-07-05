9a3e4ae6e546362cc863adc841a35aef852aabd2
Yumax-panda <yuma0wall@gmail.com>
2024-07-05 22:49:22 +0900

    skill作成のロジックを追加

--- src/app/api/_router/skills.ts
+++ src/app/api/_router/skills.ts
@@ -3,13 +3,36 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import { Hono } from 'hono'
 import { revalidateTag } from 'next/cache'
 
+import { authMiddleware } from '../_middlewares/auth'
+
 import type { Env } from '../types'
 
 import { tag } from '@/lib/routes'
-import { updateSkillSchema } from '@/models'
+import { updateSkillSchema, createSkillSchema } from '@/models'
+import { createSkill } from '@/repository/skill'
 import { updateSkillWithTagIds } from '@/usecase/skills'
 
 export const skill = new Hono<Env>()
+  .use('*', authMiddleware)
+  // POST /api/skills
+  .post('/', zValidator('json', createSkillSchema), async (c) => {
+    const skill = c.req.valid('json')
+
+    try {
+      const created = await createSkill({ ...skill, userId: c.var.user.id })
+      revalidateTag(tag.skill)
+      return c.json(created, { status: 201 })
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError && e.code === 'P2002') {
+        return c.json(
+          { error: '同じスキル名は登録できません' },
+          { status: 400 },
+        )
+      }
+      console.error('failed to create skill', e)
+      return c.json({ error: 'error' }, { status: 400 })
+    }
+  })
   // PATCH /api/skills/:skillId
   .patch('/:skillId', zValidator('json', updateSkillSchema), async (c) => {
     const skillId = c.req.param('skillId')
@@ -22,12 +45,15 @@ export const skill = new Hono<Env>()
         userId: c.var.user.id,
       })
       revalidateTag(tag.skill)
-      c.json(updated)
+      return c.json(updated)
     } catch (e) {
       if (e instanceof PrismaClientKnownRequestError && e.code === 'P2002') {
-        c.json({ error: '同じスキル名は登録できません' }, { status: 400 })
+        return c.json(
+          { error: '同じスキル名は登録できません' },
+          { status: 400 },
+        )
       }
-      c.json({ error: 'error' }, { status: 400 })
+      console.error('failed to update skill', e)
+      return c.json({ error: 'error' }, { status: 400 })
     }
   })
-// DELETE /api/skills/:skillId
