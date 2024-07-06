0907bf90845fbfa3a2b734b205e6c35dc21bcc84
Yumax-panda <yuma0wall@gmail.com>
2024-07-06 14:59:03 +0900

    tagエンドポイントを追加

--- src/app/api/core.ts
+++ src/app/api/core.ts
@@ -4,10 +4,12 @@ import { prettyJSON } from 'hono/pretty-json'
 import { handle } from 'hono/vercel'
 
 import { skill } from './_router/skills'
+import { tag } from './_router/tags'
 
 const app = new Hono()
   .basePath('/api')
   .route('/skills', skill)
+  .route('/tags', tag)
   .use('*', prettyJSON(), logger())
 
 export const handler = handle(app)
