abe61caa1b430219acfbd90197fde431e525d211
Yumax-panda <yuma0wall@gmail.com>
2024-08-07 01:11:43 +0900

    トークン更新のエンドポイントを追加

--- src/app/api/_router/v1/users.ts
+++ src/app/api/_router/v1/users.ts
@@ -1,5 +1,7 @@
 import { tag } from '@/lib/routes'
+import { updateArticleToken as updateArticleTokenSchema } from '@/models/articleToken'
 import { updateUserSchema } from '@/models/user'
+import { updateArticleToken } from '@/repository/articleToken'
 import { updateUser } from '@/repository/user'
 import { zValidator } from '@hono/zod-validator'
 import { revalidateTag } from 'next/cache'
@@ -20,6 +22,29 @@ const me = factory
       console.error('failed to update user', e)
       return new Response(null, { status: 400 })
     }
+  })
+  // PATCH /users/@me/tokens
+  .patch('/tokens', zValidator('json', updateArticleTokenSchema), async (c) => {
+    const userId = c.var.user.id
+    const tokens = c.req.valid('json')
+
+    for (const [name, token] of Object.entries(tokens)) {
+      if (name === 'QIITA' || name === 'ZENN' || name === 'NOTE') {
+        try {
+          await updateArticleToken({
+            userId,
+            provider: name,
+            token: !!token ? token : null,
+          })
+        } catch (e) {
+          console.error('failed to update article token', e)
+          return new Response(null, { status: 400 })
+        }
+      }
+    }
+
+    revalidateTag(tag.token)
+    return new Response(null, { status: 204 })
   })
 
 export const user = factory.createApp().route('/@me', me)
