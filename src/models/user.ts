e955022142b081333719985c07a21ec70c447140
Yumax-panda <yuma0wall@gmail.com>
2024-08-08 02:45:45 +0900

    エラーメッセージをAPIとUIで分けた

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,32 +1,53 @@
 import { z } from 'zod'
 
-export enum UpdateUserSchemaErrors {
-  NAME_REQUIRED = 'name is required.',
-  TOO_LONG_NAME = 'name is up to 30 characters.',
-  TOO_LONG_LOCATION = 'location is up to 100 characters.',
-  TOO_LONG_ORGANIZATION = 'organization is up to 100 characters.',
-  TOO_LONG_BIO = 'bio is up to 100 characters.',
+const NAME_REQUIRED = 'name is required.'
+const TOO_LONG_NAME = 'name is up to 30 characters.'
+const TOO_LONG_LOCATION = 'location is up to 100 characters.'
+const TOO_LONG_ORGANIZATION = 'organization is up to 100 characters.'
+const TOO_LONG_BIO = 'bio is up to 100 characters.'
+
+export const translateError = (message?: string): string | undefined => {
+  if (!message) {
+    return message
+  }
+
+  if (message.includes(NAME_REQUIRED)) {
+    return '名前を入力してください.'
+  }
+  if (message.includes(TOO_LONG_NAME)) {
+    return '名前は30文字以内で入力してください.'
+  }
+  if (message.includes(TOO_LONG_LOCATION)) {
+    return '居住地は100文字以内で入力してください.'
+  }
+  if (message.includes(TOO_LONG_ORGANIZATION)) {
+    return '所属は100文字以内で入力してください.'
+  }
+  if (message.includes(TOO_LONG_BIO)) {
+    return '自己紹介は100文字以内で入力してください.'
+  }
+  return '予期しないエラーが発生しました.'
 }
 
 export const updateUserSchema = z.object({
   name: z
     .string()
-    .min(1, UpdateUserSchemaErrors.NAME_REQUIRED)
-    .max(30, UpdateUserSchemaErrors.TOO_LONG_NAME)
+    .min(1, NAME_REQUIRED)
+    .max(30, TOO_LONG_NAME)
     .transform((v) => v.trim()),
   location: z
     .string()
-    .max(100, UpdateUserSchemaErrors.TOO_LONG_LOCATION)
+    .max(100, TOO_LONG_LOCATION)
     .nullable()
     .transform((v) => (v?.trim() ? v.trim() : null)),
   organization: z
     .string()
-    .max(100, UpdateUserSchemaErrors.TOO_LONG_ORGANIZATION)
+    .max(100, TOO_LONG_ORGANIZATION)
     .nullable()
     .transform((v) => (v?.trim() ? v.trim() : null)),
   bio: z
     .string()
-    .max(100, UpdateUserSchemaErrors.TOO_LONG_BIO)
+    .max(100, TOO_LONG_BIO)
     .nullable()
     .transform((v) => (v?.trim() ? v.trim() : null)),
 })
