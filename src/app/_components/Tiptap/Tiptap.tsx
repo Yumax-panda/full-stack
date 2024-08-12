39949810ff7b87511bd77a46a5a636145c95a386
Yumax-panda <yuma0wall@gmail.com>
2024-08-13 03:31:41 +0900

    DOM操作をできるようにrefを受け取れるように修正

--- src/app/_components/Tiptap/Tiptap.tsx
+++ src/app/_components/Tiptap/Tiptap.tsx
@@ -1,11 +1,10 @@
-'use client'
-
 import './Tiptap.css'
 
 import { Link } from '@mui/icons-material'
 import { IconButton } from '@mui/material'
 import { isTextSelection } from '@tiptap/core'
 import { BubbleMenu, EditorContent, useEditor } from '@tiptap/react'
+import { forwardRef } from 'react'
 
 import { EditorMenu } from '../EditorMenu'
 import { useBubbleMenu } from '../hooks/useBubbleMenu'
@@ -22,68 +21,68 @@ type Props = {
   editable?: boolean
 }
 
-export const Tiptap = ({
-  content,
-  onChange = () => {},
-  editable = true,
-  workId,
-  userId,
-}: Props) => {
-  const editor = useEditor({
-    content,
-    onUpdate: ({ editor }) => onChange(editor.getHTML()),
-    editable,
-    extensions,
-  })
-  const { setLink } = useBubbleMenu({ editor })
-  const { onLinkEmbedAdd, onImageAdd } = useEditorMenu({
-    editor,
-    workId,
-    userId,
-  })
+export const Tiptap = forwardRef<HTMLDivElement | null, Props>(
+  ({ content, onChange = () => {}, editable = true, workId, userId }, ref) => {
+    const editor = useEditor({
+      content,
+      onUpdate: ({ editor }) => onChange(editor.getHTML()),
+      editable,
+      extensions,
+      immediatelyRender: true,
+    })
+    const { setLink } = useBubbleMenu({ editor })
+    const { onLinkEmbedAdd, onImageAdd } = useEditorMenu({
+      editor,
+      workId,
+      userId,
+    })
 
-  return (
-    <>
-      {editor && editable && (
-        <BubbleMenu
-          editor={editor}
-          tippyOptions={{
-            placement: 'bottom',
-          }}
-          // Headingは非表示
-          // ref: https://github.com/ueberdosis/tiptap/blob/main/packages/extension-bubble-menu/src/bubble-menu-plugin.ts#L47
-          shouldShow={({ view, state, from, to }) => {
-            const { doc, selection } = state
-            const { empty } = selection
+    return (
+      <div ref={ref}>
+        {editor && editable && (
+          <BubbleMenu
+            editor={editor}
+            tippyOptions={{
+              placement: 'bottom',
+            }}
+            // Headingは非表示
+            // ref: https://github.com/ueberdosis/tiptap/blob/main/packages/extension-bubble-menu/src/bubble-menu-plugin.ts#L47
+            shouldShow={({ view, state, from, to }) => {
+              const { doc, selection } = state
+              const { empty } = selection
 
-            const isEmptyTextBlock =
-              !doc.textBetween(from, to).length &&
-              isTextSelection(state.selection)
+              const isEmptyTextBlock =
+                !doc.textBetween(from, to).length &&
+                isTextSelection(state.selection)
 
-            const element = document.querySelector('.ProseMirror')
-            const isChildOfMenu = !!element?.contains(document.activeElement)
+              const element = document.querySelector('.ProseMirror')
+              const isChildOfMenu = !!element?.contains(document.activeElement)
 
-            const hasEditorFocus = view.hasFocus() || isChildOfMenu
+              const hasEditorFocus = view.hasFocus() || isChildOfMenu
 
-            const isRangeSelected =
-              hasEditorFocus && !empty && !isEmptyTextBlock && editor.isEditable
+              const isRangeSelected =
+                hasEditorFocus &&
+                !empty &&
+                !isEmptyTextBlock &&
+                editor.isEditable
 
-            const ignoredNodes = ['heading', 'image', 'embed']
+              const ignoredNodes = ['heading', 'image', 'embed']
 
-            return (
-              !ignoredNodes.some((v) => editor.isActive(v)) && isRangeSelected
-            )
-          }}
-        >
-          <IconButton onClick={setLink}>
-            <Link />
-          </IconButton>
-        </BubbleMenu>
-      )}
-      {editor && editable && (
-        <EditorMenu onLinkEmbedAdd={onLinkEmbedAdd} onImageAdd={onImageAdd} />
-      )}
-      <EditorContent editor={editor} />
-    </>
-  )
-}
+              return (
+                !ignoredNodes.some((v) => editor.isActive(v)) && isRangeSelected
+              )
+            }}
+          >
+            <IconButton onClick={setLink}>
+              <Link />
+            </IconButton>
+          </BubbleMenu>
+        )}
+        {editor && editable && (
+          <EditorMenu onLinkEmbedAdd={onLinkEmbedAdd} onImageAdd={onImageAdd} />
+        )}
+        <EditorContent editor={editor} />
+      </div>
+    )
+  },
+)
