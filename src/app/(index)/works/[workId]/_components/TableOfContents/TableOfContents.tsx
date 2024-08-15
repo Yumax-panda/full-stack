9befd697256fa5a51b4ced090ba336d20758740e
Yumax-panda <yuma0wall@gmail.com>
2024-08-16 02:50:01 +0900

    client side navigationが日本語をエンコードしたリンクでうまく動作しないため, aタグへ置換

--- src/app/(index)/works/[workId]/_components/TableOfContents/TableOfContents.tsx
+++ src/app/(index)/works/[workId]/_components/TableOfContents/TableOfContents.tsx
@@ -1,4 +1,3 @@
-import { Link } from '@/app/_components/Link'
 import { encodeURLSafe } from '@/lib/utils'
 import { Typography } from '@mui/material'
 import { forwardRef } from 'react'
@@ -20,7 +19,7 @@ export const TableOfContents = forwardRef<HTMLUListElement | null, Props>(
         </Typography>
         <ul ref={ref} className={styles.toc}>
           {headings.map(({ level, text }, i) => (
-            <Link
+            <a
               key={`heading-${i}-${text}`}
               href={`#${encodeURLSafe(text)}`}
               className={styles.link}
@@ -35,7 +34,7 @@ export const TableOfContents = forwardRef<HTMLUListElement | null, Props>(
               >
                 {text}
               </li>
-            </Link>
+            </a>
           ))}
         </ul>
       </CardContainer>
